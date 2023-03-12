c7a2ee9bf2675e82f667abac1c34778e68977103
shawn-coding <shawncode484@gmail.com>
2023-03-11 21:34:21 -0500

    Initial Commit of Spring Boot Initializr project with Web Dependency

---
+++ src/main/java/com/shawncoding/grognaksmeats/GrognaksMeatsApplication.java
@@ -0,0 +1,13 @@
+package com.shawncoding.grognaksmeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrognaksMeatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrognaksMeatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shawncoding/grognaksmeats/GrognaksMeatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shawncoding.grognaksmeats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrognaksMeatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
